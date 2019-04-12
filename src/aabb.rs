b6e3ca9405aff88b1265df24c41a9e78c8596d3b
Cliff L. Biffle <code@cliffle.com>
2019-04-12 09:14:55 -0700

    Vectorize AABB hit test.
    This expresses the iterative version in terms of vector math, which
    looks like it would do more work...except that it auto-vectorizes to AVX
    instructions. It's considerably faster.

--- src/aabb.rs
+++ src/aabb.rs
@@ -15,35 +15,16 @@ impl Aabb {
         }
     }
 
-    pub fn hit(&self, ray: &Ray, mut t_range: std::ops::Range<f32>) -> bool {
-        use crate::vec3::Axis::*;
-
-        for &axis in &[X, Y, Z] {
-            let inv_d = 1. / ray.direction[axis];
-            let t0 = (self.min[axis] - ray.origin[axis]) * inv_d;
-            let t1 = (self.max[axis] - ray.origin[axis]) * inv_d;
-            let (t0, t1) = if inv_d < 0. { (t1, t0) } else { (t0, t1) };
-            t_range.start = t_range.start.max(t0);
-            t_range.end = t_range.end.min(t1);
-            if t_range.end <= t_range.start {
-                return false;
-            }
-        }
-        true
-    }
-
-    // experimental, totally untested, vector rewrite of the above algorithm
-    pub fn hit_v(&self, ray: &Ray, mut t_range: std::ops::Range<f32>) -> bool {
+    pub fn hit(&self, ray: &Ray, t_range: std::ops::Range<f32>) -> bool {
         let inv_d = ray.direction.map(|x| 1. / x);
         let t0 = (self.min - ray.origin) * inv_d;
         let t1 = (self.max - ray.origin) * inv_d;
-
-        let t0_ = inv_d.zip_with3(t0, t1, |i, a, b| if i < 0. { b } else { a });
-        let t1_ = inv_d.zip_with3(t1, t0, |i, a, b| if i < 0. { b } else { a });
-
-        t_range.start = t_range.start.max(t0_.reduce(f32::max));
-        t_range.end = t_range.end.max(t1_.reduce(f32::min));
-
-        t_range.end > t_range.start
+        let (t0, t1) = (
+            inv_d.zip_with3(t0, t1, |i, a, b| if i < 0. { b } else { a }),
+            inv_d.zip_with3(t0, t1, |i, a, b| if i < 0. { a } else { b }),
+        );
+        let start = t_range.start.max(t0.reduce(f32::max));
+        let end = t_range.end.min(t1.reduce(f32::min));
+        end > start
     }
 }
