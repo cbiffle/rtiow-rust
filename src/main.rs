80aacdeebcba919b469839b9d81a7a707a532d6f
Cliff L. Biffle <code@cliffle.com>
2019-04-14 11:24:48 -0700

    Implement final scene from second book.

--- src/main.rs
+++ src/main.rs
@@ -138,11 +138,176 @@ fn simple_light_scene(
                 material: Material::Lambertian {
                     albedo: rtiow::texture::constant(Vec3::from(0.3)),
                 },
-                motion: Vec3::default(),
             },
             offset: 277. + 257. * rng.gen::<Vec3>(),
         }));
     }
+
+    world.push(Box::new(object::FlipNormals(object::Sphere {
+        radius: 1000.,
+        material: Material::DiffuseLight {
+            emission: rtiow::texture::constant(Vec3::from(0.1)),
+            brightness: 1.,
+        },
+    })));
+
+    (world, camera, exposure)
+}
+
+fn book_final_scene(
+    nx: usize,
+    ny: usize,
+    rng: &mut impl Rng,
+) -> (Vec<Box<dyn Object>>, Camera, Range<f32>) {
+    let look_from = Vec3(478., 278., -600.);
+    let look_at = Vec3(278., 278., 0.);
+    let dist_to_focus = 10.;
+    let aperture = 0.0;
+    let exposure = 0. ..1.;
+
+    let camera = Camera::look(
+        look_from,
+        look_at,
+        Vec3(0., 1., 0.),
+        40.,
+        nx as f32 / ny as f32,
+        aperture,
+        dist_to_focus,
+        exposure.clone(),
+    );
+
+    use rtiow::material::Material;
+    use rtiow::texture;
+
+    let ground = Material::Lambertian {
+        albedo: texture::constant(Vec3(0.48, 0.83, 0.53)),
+    };
+
+    let mut world: Vec<Box<dyn Object>> = vec![];
+
+    // Make random floor.
+    for i in 0..20 {
+        for j in 0..20 {
+            const W: f32 = 100.;
+            let c0 = Vec3(-1000. + i as f32 * W, 0., -1000. + j as f32 * W);
+            let c1 = c0 + Vec3(W, 100. * (rng.gen::<f32>() + 0.01), W);
+            world.push(Box::new(object::rect_prism(c0, c1, ground.clone())));
+        }
+    }
+
+    // Make light.
+    world.push(Box::new(object::Rect {
+        orthogonal_to: object::StaticY,
+        range0: 123. ..423.,
+        range1: 147. ..412.,
+        k: 554.,
+        material: Material::DiffuseLight {
+            emission: texture::constant(Vec3::from(1.)),
+            brightness: 7.,
+        },
+    }));
+
+    // Brown blurry sphere.
+    world.push(Box::new(object::Translate {
+        offset: Vec3(400., 400., 200.),
+        object: object::LinearMove {
+            motion: Vec3(30., 0., 0.),
+            object: object::Sphere {
+                radius: 50.,
+                material: Material::Lambertian {
+                    albedo: texture::constant(Vec3(0.7, 0.3, 0.1)),
+                },
+            },
+        },
+    }));
+
+    let glass = Material::Dielectric { ref_idx: 1.5 };
+
+    // Glass sphere.
+    world.push(Box::new(object::Translate {
+        offset: Vec3(260., 150., 45.),
+        object: object::Sphere {
+            radius: 50.,
+            material: glass.clone(),
+        },
+    }));
+
+    // Silvery sphere.
+    world.push(Box::new(object::Translate {
+        offset: Vec3(0., 150., 145.),
+        object: object::Sphere {
+            radius: 50.,
+            material: Material::Metal {
+                albedo: Vec3(0.8, 0.8, 0.9),
+                fuzz: 10.,
+            },
+        },
+    }));
+
+    // Blue glass sphere.
+    let boundary = object::Translate {
+        offset: Vec3(360., 150., 145.),
+        object: object::Sphere {
+            radius: 70.,
+            material: glass.clone(),
+        },
+    };
+    world.push(Box::new(boundary.clone()));
+    world.push(Box::new(object::ConstantMedium {
+        boundary,
+        density: 0.2,
+        material: Material::Isotropic {
+            albedo: texture::constant(Vec3(0.2, 0.4, 0.9)),
+        },
+    }));
+
+    // Fog.
+    world.push(Box::new(object::ConstantMedium {
+        boundary: object::Sphere {
+            radius: 5000.,
+            material: glass.clone(), // doesn't matter
+        },
+        density: 0.0001,
+        material: Material::Isotropic {
+            albedo: texture::constant(Vec3::from(1.)),
+        },
+    }));
+
+    // Perlin marbled sphere.
+    world.push(Box::new(object::Translate {
+        offset: Vec3(220., 280., 300.),
+        object: object::Sphere {
+            radius: 80.,
+            material: Material::Lambertian {
+                albedo: texture::perlin(0.05),
+            },
+        },
+    }));
+
+    // Cube made of random spheres.
+    world.push(Box::new({
+        const SPHERES: usize = 1000;
+        let white = Material::Lambertian {
+            albedo: texture::constant(Vec3::from(0.73)),
+        };
+        let spheres = (0..SPHERES).map(|_| {
+            Box::new(object::Translate {
+                offset: 165. * rng.gen::<Vec3>(),
+                object: object::Sphere {
+                    radius: 10.,
+                    material: white.clone(),
+                },
+            }) as Box<dyn Object>
+        }).collect();
+        let bvh = rtiow::bvh::from_scene(spheres, exposure.clone(), rng);
+        object::Translate {
+            offset: Vec3(-100., 270., 395.),
+            object: object::rotate_y(
+                15.,
+                bvh,
+            ),
+        }
+    }));
 
     (world, camera, exposure)
 }
@@ -163,7 +328,8 @@ fn main() {
 
     //let (world, camera, exposure) = cornell_box_scene(NX, NY);
     //let (world, camera, exposure) = simple_light_scene(NX, NY, &mut rng);
-    let (world, camera, exposure) = volume_test(NX, NY);
+    //let (world, camera, exposure) = volume_test(NX, NY);
+    let (world, camera, exposure) = book_final_scene(NX, NY, &mut rng);
 
     let (image, time) = if USE_BVH {
         eprintln!("Generating bounding volume hierarchy.");
--- src/texture.rs
+++ src/texture.rs
@@ -21,6 +21,6 @@ pub fn checker(t0: Texture, t1: Texture) -> Texture {
 }
 
 pub fn perlin(scale: f32) -> Texture {
-    //Arc::new(move |p| Vec3::from(perlin::turb(scale * p, 7)))
-    Arc::new(move |p| Vec3::from(0.5 * (1. + f32::sin(scale * p.2 + 10. * perlin::turb(p, 7)))))
+    Arc::new(move |p| Vec3::from(perlin::turb(scale * p, 7)))
+    //Arc::new(move |p| Vec3::from(0.5 * (1. + f32::sin(scale * p.0 + 5. * perlin::turb(scale * p, 7)))))
 }
